9ac55fcec8dc95db37728293c2ec22b6a6d2e887
Muhammad Sulaiman <sulaimanperson@gmail.com>
2022-01-09 14:06:46 +0500

    all set and done

--- pages/[singlecurrency].js
+++ pages/[singlecurrency].js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Router from 'next/router'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Head from 'next/head'
 
 // {`${getCoin.name.replace(/\s+/g, '-')}`}
 
@@ -51,6 +52,13 @@ const SingleCurrency = ({data}) => {
 
     return (
         <>
+         <Head>
+      <title>{data.name}</title>
+      <link rel="icon" href={data.image.large} />
+      <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no"/>
+
+    </Head>
+
          <div className="singleCurrencyCont" style={data.description.en === '' ? {paddingBottom: '30px'} : {paddingBottom: '0px'}}>
          <div onClick={goBack} className="goBackBtn" title="Go back"><svg aria-hidden="true" width="16px" focusable="false" data-prefix="fas" data-icon="chevron-left" className="svg-inline--fa fa-chevron-left" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="white" d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"></path></svg></div>
          <div className="rankSingleCurrency" >{`Rank #${data.market_cap_rank}`}</div>
